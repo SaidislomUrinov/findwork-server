43dccff3b68d36311da83b98736c829837f967ef
SaidislomUrinov <me@saidnet.uz>
2025-02-06 17:06:31 +0500

    app cleaned

--- app.js
+++ app.js
@@ -5,18 +5,10 @@ import mongoose from 'mongoose';
 import { MONGO_URI, PORT } from './src/utils/env.js';
 import fileUpload from 'express-fileupload';
 import router from './src/router.js';
-import { sendActivator } from './src/utils/email.js';
 mongoose.connect(MONGO_URI);
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 app.use('/api', router);
-app.listen(PORT, async ()=>{
-    try {
-        // const ok = await sendActivator('getmebroo@gmail.com', 'salom');
-        // console.log(ok)
-    } catch (error) {
-        console.log(error.message)
-    }
-});
+app.listen(PORT);
